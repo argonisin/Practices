19d4b0f82421bf1a181a53dae7bcb044949d8971
Argon <167205269+argonisin@users.noreply.github.com>
2024-10-04 17:50:03 +0800

    correction with fix

--- Randos/addrStrConv.py
+++ Randos/addrStrConv.py
@@ -83,9 +83,10 @@ class encoder:
     
     # Encode Method
     def encode(self):
-        """For the value of the hex__ variable:
-           First we translate the decimal into binary, and then that binary to decimal and then octadecimal,
-           and then finally that octadecimal to decimal and to hexidecimal.  
+        """
+           For the value of the 'hex__' variable:
+           First we translate the decimal into binary, and then that binary value gets data casted to INT and then
+           converted to octadecimal, and finally that octadecimal again gets data casted to INT and converted to hexidecimal.  
         """
         hex__ = (hex(int(oct(int(bin(self.lenStr)[2:]))[2:])))
         
